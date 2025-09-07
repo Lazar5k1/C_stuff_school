9ef133db812f137037c5775c03da03d2a1e6fe62
Lazar5k1 <nicholas.gill@outlook.com>
2025-09-07 01:15:56 -0400

    write more to later_tester.c

--- later_tester.c
+++ later_tester.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>                 // you can use %s and char arrays without this but you need string.h for string functions
 
 int main(){
     int dolphin[10][10] = {{-1, -1, -1, -1, -1}, {-1, -1, -1, -1 , -1}};
@@ -25,11 +26,20 @@ int main(){
     char gator[] = "apple";
     printf("%s\n", gator);
     char mountain[5];
-    fgets(mountain, sizeof(mountain), stdin);               // when using scanf it's stop at the first white space, fgets gets all input within the second parameter's limit.
+    fgets(mountain, sizeof(mountain), stdin);               // when using scanf it stops at the first white space, fgets gets all input within the second parameter's limit.
     printf("%s\n", mountain);
     printf("sizeof(mountain = %d)\n", sizeof(mountain));
     printf("sizeof(gator = %d)\n", sizeof(gator));          // when declaring a string it must be the length of the word + 1 because the \0 counts as a character
-
+    char title[] = "soul";
+    printf("strlen(title) = %zu\n", strlen(title));            // counts characters in string and ignores \0
+    char newGame[strlen(title) + 1];                        // if you don't put the + 1 it still "works" but that would be called a buffer overflow :o
+    strcpy(newGame, title);                                        // also apparently you can exploit buffer overflow with malicious code??? super cool stuff dude
+    printf("newGame = %s\n");
+    char swim[] = "never";
+    strcat(title, swim);
+    printf("title = %s\n", title);
+    char stairs[] = "never";
+    
 
     return 0;
 }
